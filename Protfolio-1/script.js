1a4ff020cca91ba2e3e8a017fc290dbfb10b42eb
Pema@777 <pemanamgurtamang7@gmail.com>
2023-06-14 10:58:01 +0545

    first commit

---
+++ Protfolio-1/script.js
@@ -0,0 +1,30 @@
+console.log("Hello World");
+
+let elem = document.getElementsByClassName('btn');
+elem[0].classList.add('btn-click');
+
+
+function myFunction(){
+let a = prompt("Can i  get your experienced ");
+let = parseInt(a);
+switch (a) {
+    case 1:
+        console.log("Acceptable");
+        break;
+    case 2:
+        console.log("Good");
+        break;
+    case 3:
+        console.log("Acceptable");
+        break;
+    case 4:
+        console.log("Good");
+        break;
+    default:
+        console.log("you are accepted");
+        break;
+}
+return ques;
+}
+
+//let call = myFunction();
